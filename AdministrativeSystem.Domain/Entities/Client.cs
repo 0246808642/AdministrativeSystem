1e2eb4afc45c861cf29b423208dbf14b6b8b9df5
caique <caiquepinheiro69@gmail.com>
2026-02-06 09:36:17 -0300

    Criado as entidades

---
+++ AdministrativeSystem.Domain/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿namespace AdministrativeSystem.Domain.Entities;
+
+public class Client
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    
+    protected Client() {} // EF
+
+    public Client(string name, string email)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Email = email;
+        IsActive = true;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
+    }
+}
---
+++ AdministrativeSystem.Domain/Entities/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿namespace AdministrativeSystem.Domain.Entities;
+
+public class OrderItem
+{
+    public Guid Id { get; set; }
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    
+    protected OrderItem() {}
+
+    internal OrderItem(Guid productId, int quantity, decimal unitPrice)
+    {
+        if(quantity <= 0)
+            throw new ArgumentException("Quantity must be greater than or equal to 0.");
+        
+        Id = Guid.NewGuid();
+        ProductId = productId;
+        Quantity = quantity;
+        UnitPrice = unitPrice;
+    }
+
+    public decimal CalculateTotalPrice()
+    {
+        return UnitPrice * Quantity;
+    }
+}
